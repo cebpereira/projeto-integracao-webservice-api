c06875e14601ace2b5610ae9158e2a619f6067db
Carlos Elandro <60947051+cebpereira@users.noreply.github.com>
2022-10-04 22:29:05 -0300

    Add files via upload

---
+++ web/build/precache-manifest.efd17cdeb115d5cccda3890cfb6682c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551419bed67ea863dc08c2552b624ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b95393cd0df9a557962",
+    "url": "/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "abb43216652b69a1f670",
+    "url": "/static/css/main.bbf464e9.chunk.css"
+  },
+  {
+    "revision": "3b95393cd0df9a557962",
+    "url": "/static/js/2.3edf5928.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3edf5928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb43216652b69a1f670",
+    "url": "/static/js/main.4282c901.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
